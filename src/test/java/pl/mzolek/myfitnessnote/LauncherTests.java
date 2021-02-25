1fb9565becbaa8204ee26c68aabe6f77ff74a19a
Marek Żołek <mzolek93@gmail.com>
2021-02-25 20:33:15 +0100

    update v3

--- src/test/java/pl/mzolek/myfitnessnote/LauncherTests.java
+++ src/test/java/pl/mzolek/myfitnessnote/LauncherTests.java
@@ -1,11 +1,16 @@
 package pl.mzolek.myfitnessnote;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class LauncherTests {
 
     @Test
