a143caa31189e426d7dc7b3f5505e78340062867
Marek Żołek <mzolek93@gmail.com>
2021-02-25 19:47:38 +0100

    update for CI

--- src/test/java/pl/mzolek/myfitnessnote/LauncherTests.java
+++ src/test/java/pl/mzolek/myfitnessnote/LauncherTests.java
@@ -3,11 +3,14 @@ package pl.mzolek.myfitnessnote;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class LauncherTests {
 
     @Test
     void contextLoads() {
+        assertTrue( true );
     }
 
 }
